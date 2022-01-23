c6754a30d97f49b40651c3c61cc39a7e113a3b29
DESKTOP-MTGVP5G\DELL <4146kt@gmail.com>
2022-01-23 09:54:13 +0900

    Java 예제 풀이

---
+++ Java/CodeUp100문제/Main5.java
@@ -0,0 +1,23 @@
+package codeUpExample;
+/*이번에는 큰따옴표(double quotation mark)가 포함된 출력문을 연습해보자.
+	다음 문장을 출력하시오.
+	
+	"Hello World"
+	(단, 큰따옴표도 함께 출력한다.)
+	
+	참고
+	printf( ) 함수로 큰 따옴표 문자 " 를 출력하기 위해서는 \" 로 출력해야 한다.
+	
+	예시
+	printf("\"");
+	
+	** 주의 : 본 화면에서 복사하여 붙여넣기하면 제대로 되지 않을 수 있으니 직접 작성해야한다.
+ */
+public class Main5 {
+
+	public static void main(String[] args) {
+		System.out.println("\"Hello World\"");
+	}
+
+}
+
