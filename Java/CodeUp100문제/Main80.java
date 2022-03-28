001966a7d3dd065962796aae1d5aeae951ffeb78
DESKTOP-MTGVP5G\DELL <4146kt@gmail.com>
2022-03-28 22:00:12 +0900

    22.03.28 Java Study
    아직 풀이가 완료되지 않았습니다.

---
+++ Java/CodeUp100문제/Main80.java
@@ -0,0 +1,32 @@
+package codeUpExample;
+/*
+1, 2, 3 ... 을 계속 더해 나갈 때,
+그 합이 입력한 정수(0 ~ 1000)보다 같거나 작을 때까지
+계속 더하는 프로그램을 작성해보자.
+
+즉, 1부터 n까지 정수를 계속 더한다고 할 때,
+어디까지 더해야 입력한 수보다 같거나 커지는지 알아보고자 하는 문제이다.
+*/
+import java.util.Scanner;
+
+public class Main80 {
+	
+	static int count;
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		sc.close();
+		int sum = 0;
+
+		
+		for(int i=1; sum<=x; i++) {
+			sum += i;
+			count = i;
+		}
+		
+		System.out.println(count);
+		
+	}
+}
